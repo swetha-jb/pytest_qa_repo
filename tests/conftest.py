41f8cb3a2a52ca38eca656cf5a21658e444825eb
swetha <swetha.j@vvdntech.in>
2025-07-22 12:29:09 +0530

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,27 @@
 import time
 import pytest
-from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import tempfile
 
 @pytest.fixture()
 def setup(request):
-    driver = webdriver.Chrome()
-    driver.get("https://www.flipkart.com/")
+    # Create a temporary user data directory
+    user_data_dir = tempfile.mkdtemp()
 
-    # driver.get("http://192.168.1.1/start.htm")
-    driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")  # Optional: run without GUI
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.flipkart.com/")
     time.sleep(5)
     request.cls.driver = driver
     yield
     driver.quit()
+
