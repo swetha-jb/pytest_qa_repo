2baafcad5316118a90e5a978bc6a1b26dddba73f
swetha <swetha.j@vvdntech.in>
2025-07-22 13:59:44 +0530

    testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,12 @@ import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import tempfile
 
 @pytest.fixture()
 def setup(request):
-    # Create a temporary user data directory
-    user_data_dir = tempfile.mkdtemp()
-
+   
     chrome_options = Options()
-    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+  
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-extensions")
